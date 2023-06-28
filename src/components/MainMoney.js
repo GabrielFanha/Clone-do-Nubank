7a931ff1cc993919df7b5f9ca8784c6a66244fb2
GabrielFanha <gabriel.lfanha@outlook.com>
2023-06-28 06:08:39 -0300

    add icon mainmoney

--- src/components/MainMoney.js
+++ src/components/MainMoney.js
@@ -1,14 +1,22 @@
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity } from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
 
 export default function MainMoney() {
   return (
-    <View className="p-6">
-      <Text className="font-sans font-medium text-2xl text-black dark:text-white">
-        Conta
-      </Text>
-      <Text className="font-sans font-bold text-3xl text-black dark:text-white">
-        R$ 3.249,00
-      </Text>
-    </View>
+    <TouchableOpacity className="p-6 flex-row justify-between items-center">
+      <View>
+        <Text className="font-sans font-medium text-2xl text-black dark:text-white">
+          Conta
+        </Text>
+        <Text className="font-sans font-bold text-3xl text-black dark:text-white">
+          R$ 3.249,00
+        </Text>
+      </View>
+      <Icon
+        name="chevron-right"
+        size={24}
+        className="text-black dark:text-white"
+      />
+    </TouchableOpacity>
   )
 }
